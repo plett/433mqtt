08a9a3efc49ff95addabf9553746c5fc5d14168c
Paul Lettington <paul@plett.co.uk>
2018-02-17 23:25:29 +0000

    Initial version

---
+++ 433mqtt.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import paho.mqtt.client as mqtt
+import json
+import sys
+import os
+
+host = os.environ.get('RTL433MQTTHOST')
+port = int(os.environ.get('RTL433MQTTPORT', 1833))
+username = os.environ.get('RTL433MQTTUSER','rtl_433')
+password = os.environ.get('RTL433MQTTPASS')
+topic = os.environ.get('RTL433MQTTTOPIC','/rtl_433')
+
+#fields which might contain a device id
+idfields= [
+    'id', #most devices
+    'dev_id', #currentcost
+    'device', #ambient_weather vaillant_vrt340f
+    'sensor_id', #accurite
+    'node', #emontx
+    'device id', #hondaremote
+    'address', #ht680
+    'sid' #oregon_scientific springfield
+    ]
+
+if __name__ == "__main__":
+    client = mqtt.Client()
+    client.username_pw_set(username,password)
+    client.tls_set()
+    client.connect(host,port)
+    while 1:
+        try:
+            line = sys.stdin.readline().rstrip()
+        except KeyboardInterrupt:
+            break
+        if not line:
+            break
+
+        try:
+            jsonline = json.loads(line)
+        except ValueError:
+            # this is not a valid line of json, skip it
+            continue
+
+        #find if we have a model name
+        model = 'unknown'
+        if 'model' in jsonline:
+            model = str(jsonline['model'])
+
+        # find if we have an id field
+        idname='unknown'
+        for idfield in idfields:
+            if idfield in jsonline:
+                idname = str(jsonline[idfield])
+                break
+
+        #print(model,'/',idname,'=', line)
+        fulltopic = ''+topic+'/'+model+'/'+idname
+        client.publish(fulltopic,line)
+        client.loop(0)
+
+    client.disconnect()
---
+++ README.md
@@ -0,0 +1,24 @@
+Small program to join rtl_433 to MQTT
+
+There are many tiny scripts to take the output of rtl_433 and pipe it into
+MQTT, they all push to a single topic. I have a few different 433MHz sensors
+which I want to read in Home Assistant and writing Home Assistant template
+rules to pull the two apart isn't fun.
+
+
+Sample input:
+{"time" : "2018-02-17 22:11:13", "model" : "CurrentCost TX", "dev_id" : 77, "power0" : 517, "power1" : 0, "power2" : 0}
+{"time" : "2018-02-17 22:11:20", "model" : "CurrentCost TX", "dev_id" : 77, "power0" : 521, "power1" : 0, "power2" : 0}
+{"time" : "2018-02-17 22:11:25", "model" : "CurrentCost TX", "dev_id" : 77, "power0" : 525, "power1" : 0, "power2" : 0}
+{"time" : "2018-02-17 22:11:31", "model" : "CurrentCost TX", "dev_id" : 77, "power0" : 521, "power1" : 0, "power2" : 0}
+{"time" : "2018-02-17 22:11:37", "model" : "Kerui PIR Sensor", "id" : 468438, "data" : "0xa (PIR)"}
+{"time" : "2018-02-17 22:11:49", "model" : "CurrentCost TX", "dev_id" : 77, "power0" : 524, "power1" : 0, "power2" : 0}
+{"time" : "2018-02-17 22:11:55", "model" : "CurrentCost TX", "dev_id" : 77, "power0" : 525, "power1" : 0, "power2" : 0}
+{"time" : "2018-02-17 22:12:01", "model" : "CurrentCost TX", "dev_id" : 77, "power0" : 522, "power1" : 0, "power2" : 0}
+
+Run rtl_433 and 433mqtt as:
+RTL433MQTTHOST=myhost RTL433MQTTUSER=myuser RTL433MQTTPASS=mypassword rtl_433 -q -F json -U | 433mqtt.py
+
+TODO:
+Dedupe messages - Kerui PIR sends its message lots of times to make sure it
+gets through
